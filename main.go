8f8539433e52622241609b0caebd8071120cab93
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-11-04 22:09:14 -0300

    feat: receive and print files through multipart/form

--- main.go
+++ main.go
@@ -1,14 +1,51 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"log"
+	"mime"
+	"mime/multipart"
 	"net/http"
+	"strings"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
     if (r.Method != http.MethodPost) {
         http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
     }
+
+    contentType := r.Header.Get("Content-type")
+    mediaType, params, err := mime.ParseMediaType(contentType)
+
+    if (err != nil) {
+        log.Fatal(err)
+    }
+
+    if (!strings.HasPrefix(mediaType, "multipart/")) {
+        log.Fatal("Unsupported content-type")
+    }
+
+    mr := multipart.NewReader(r.Body, params["boundary"])
+    var content []byte
+    for {
+        p, err := mr.NextPart()
+
+        if (err != nil) {
+            break
+        }
+
+        slurp, err := io.ReadAll(p)
+        if (err != nil) {
+            log.Fatal(err)
+        }
+
+        content = append(content, slurp[:]...)
+    }
+
+    str := bytes.NewBuffer(content).String()
+    fmt.Printf("content: %s\n", str);
 }
 
 func main() {
